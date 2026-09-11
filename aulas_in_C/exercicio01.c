b94c3b375e139e4b8a0689d9a32c2b0c1261b7d5
jammyl almeida <fsociety404.1111@gmail.com>
2026-09-11 19:46:00 -0300

    Crie um algoritmo para mostrar a frase classica.

--- aulas_in_C/exercicio01.c
+++ aulas_in_C/exercicio01.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
 int main(){
-      float salary, aumento, salary_final;
-      printf("Informe seu salario: R$");
-      scanf("%f", &salary);
-      printf("Informe o aumento em %%: -> ");
-      scanf("%f", &aumento);
-      salary_final = salary + ((salary * aumento) / 100);
-      printf("O seu salario antigo era: R$%.2f reais.\n", salary);
-      printf("Apos ganha %.2f%% de aumento, vale: R$%.2f reais.\n", aumento, salary_final);
+      printf("Hello World!\n");
       return 0;
 }
---
+++ aulas_in_C/exercicio02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+      char name[20];
+      printf("Informe o seu nome: ");
+      scanf("%s", &name);
+      printf("E um prazer em te conhecer, %s!\n", name);
+      return 0;
+}
