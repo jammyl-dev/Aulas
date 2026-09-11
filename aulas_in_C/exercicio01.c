affce3b77d83edf1e51a12936e755c66a57ea43b
jammyl almeida <fsociety404.1111@gmail.com>
2026-09-11 16:26:12 -0300

    Update exercicio01.c

--- aulas_in_C/exercicio01.c
+++ aulas_in_C/exercicio01.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(){
-      printf("Hello, World!\n");
+      int n1, n2;
+      printf("Informe o primeiro valor: ");
+      scanf("%d", &n1);
+      printf("Informe o segundo valor: ");
+      scanf("%d", &n2);
+      printf("A soma entre %d e %d, vale: %d.\n", n1, n2, n1 + n2);
       return 0;
 }
