de25db1a3ec30832ec3c17193f7ff9699094ba07
jammyl almeida <fsociety404.1111@gmail.com>
2026-09-11 21:13:22 -0300

    Tabuada.

---
+++ aulas_in_C/exercicio08.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+      int n;
+      printf("Informe um numero: ");
+      scanf("%d", &n);
+      printf("==============\n");
+      printf("%d X %.0d = %.0d.\n", n, 1, n * 1);
+      printf("%d X %.0d = %.0d.\n", n, 2, n * 2);
+      printf("%d X %.0d = %.0d.\n", n, 3, n * 3);
+      printf("%d X %.0d = %.0d.\n", n, 4, n * 4);
+      printf("%d X %.0d = %.0d.\n", n, 5, n * 5);
+      printf("%d X %.0d = %.0d.\n", n, 6, n * 6);
+      printf("%d X %.0d = %.0d.\n", n, 7, n * 7);
+      printf("%d X %.0d = %.0d.\n", n, 8, n * 8);
+      printf("%d X %.0d = %.0d.\n", n, 9, n * 9);
+      printf("%d X %.0d = %.0d.\n", n, 10, n * 10);
+      printf("==============\n");
+      return 0;
+}
